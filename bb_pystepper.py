557b3626b7dadce1788e2630de3950fbfd5d7277
Valerie Magri <3lucky10@gmail.com>
2013-09-24 19:12:50 -0400

    PEP8 fixes

--- bb_pystepper.py
+++ bb_pystepper.py
@@ -1,5 +1,5 @@
-""" 
-bb_pystepper is a Python module used to control a stepper motor via the 
+"""
+bb_pystepper is a Python module used to control a stepper motor via the
 BeagleBone
 """
 
@@ -13,16 +13,19 @@ def initialize_pins(pins):
     for pin in pins:
         GPIO.setup(pin, GPIO.OUT)
 
+
 def set_all_pins_low(pins):
     for pin in pins:
         GPIO.output(pin, GPIO.LOW)
-        
+
+
 def wavedrive(pins, pin_index):
     for i in range(len(pins)):
         if i == pin_index:
             GPIO.output(pins[i], GPIO.HIGH)
         else:
             GPIO.output(pins[i], GPIO.LOW)
+
 
 def fullstep(pins, pin_index):
     """pin_index is the lead pin"""
@@ -33,59 +36,55 @@ def fullstep(pins, pin_index):
 
 
 class Stepper(object):
-    def __init__(self, steps_per_rev = 2048.0,
-                 pins = ["P8_13", "P8_14", "P8_15", "P8_16"]):
+    def __init__(self, steps_per_rev=2048.0,
+                 pins=["P8_13", "P8_14", "P8_15", "P8_16"]):
 
         self.pins = pins
-        
+
         initialize_pins(self.pins)
         set_all_pins_low(self.pins)
-        
+
         self.angle = 0
         self.steps_per_rev = steps_per_rev
-        
+
         # Initialize stepping mode
         self.drivemode = fullstep
-    
+
     def rotate(self, degrees=360, rpm=15):
         step = 0
-        
+
         # Calculate time between steps in seconds
         wait_time = 60.0/(self.steps_per_rev*rpm)
-        
+
         # Convert degrees to steps
         steps = math.fabs(degrees*self.steps_per_rev/360.0)
         self.direction = 1
-        
+
         if degrees < 0:
             self.pins.reverse()
             self.direction = -1
-        
+
         while step < steps:
             for pin_index in range(len(self.pins)):
                 self.drivemode(self.pins, pin_index)
                 time.sleep(wait_time)
                 step += 1
-                self.angle = (self.angle + self.direction/self.steps_per_rev \
-                *360.0) % 360.0
-        
+                self.angle = (self.angle + self.direction/self.steps_per_rev
+                              * 360.0) % 360.0
+
         if degrees < 0:
-    		self.pins.reverse()
-    	
+            self.pins.reverse()
+
         set_all_pins_low(self.pins)
-        
+
     def zero_angle(self):
         self.angle = 0
-        
+
 
 def main():
     stepper = Stepper()
     stepper.rotate()
-    
+
 
 if __name__ == "__main__":
     main()
-    
-    
-    
-    
