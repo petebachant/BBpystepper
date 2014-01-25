0250c759b25c2275a92c70c14f9de7a8aa8f0acd
Pete Bachant <petebachant@gmail.com>
2014-01-24 23:18:26 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+    name='BBpystepper',
+    version='0.0.1',
+    author='Pete Bachant',
+    author_email='petebachant@gmail.com',
+    packages=['bbpystepper'],
+    scripts=[],
+    url='https://github.com/petebachant/BBpystepper.git',
+    license='LICENSE',
+    description='Module for stepper motor control with the BeagleBone Black',
+    long_description=open('README.md').read(),
+)
